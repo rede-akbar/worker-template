79674348b5880741b40aac80b897f654a0b6c5d1
flyingdutchman <rede.akbar@loratechai.com>
2023-03-11 19:14:34 +0700

    queue

--- config/dev.py
+++ config/dev.py
@@ -3,7 +3,7 @@ from .base import BaseConfig
 
 class DevConfig(BaseConfig):
     task_default_exchange: str = "dev"
-    task_default_queue: str = "dev-master"
+    task_default_queue: str = "dev-puller"
 
 
 locals().update(DevConfig().dict(exclude_none=True))
